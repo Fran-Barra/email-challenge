8e3d9ae17918a0a2cedc722c4bb97eef4ab9bd9a
Franco B. <franco.barravecchia2002@gmail.com>
2024-05-06 13:40:18 -0300

    feat: MailSender interface

---
+++ src/service/mailSender/MailSender.ts
@@ -0,0 +1,18 @@
+
+export interface MailSender {
+    sendEmail(mailData: MailData) : Promise<Response>
+}
+
+export class MailData {
+    readonly from: String;
+    readonly too: String[];
+    readonly subject: String;
+    readonly content: String;
+
+    constructor(from: String, too: String[], subject: String, content: String) {
+        this.from = from;
+        this.too = too;
+        this.subject = subject;
+        this.content = content;
+    }
+}
