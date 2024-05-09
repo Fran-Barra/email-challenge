6f4d01c405b2875e05a1e340af633c5057613fd1
Franco B. <franco.barravecchia2002@gmail.com>
2024-05-09 13:04:49 -0300

    feat: using sendgrid in test

---
+++ src/service/mailSender/providers/sendgridProvider.ts
@@ -0,0 +1,19 @@
+import * as sng from '@sendgrid/mail';
+import {EmailProvider, EmailResponse, MailData} from '../EmailProvider';
+
+export class SendgridEmailProvider implements EmailProvider {
+    constructor() {
+        const key = process.env.SENDGRID_API_KEY;
+        if (key !== undefined) sng.setApiKey(key);
+    }
+
+    async sendEmail(mailData: MailData): Promise<EmailResponse> {
+        const sngResponse: [sng.ClientResponse, {}] = await sng.sendMultiple({
+            to: mailData.to,
+            from: mailData.from,
+            subject: mailData.subject,
+            html: mailData.content,
+        });
+        return {status: sngResponse[0].statusCode};
+    }
+}
