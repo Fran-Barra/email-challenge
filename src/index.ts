fbfae6a4103e562392df6119a7bd56a84dd2c5dd
Franco B. <franco.barravecchia2002@gmail.com>
2024-05-21 11:45:50 -0300

    update: use .env in index

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as morgan from 'morgan';
 import * as dotenv from 'dotenv';
 
 //TODO: change considering NODE_ENV
-dotenv.config({path: '.env.dev'});
+dotenv.config({path: '.env'});
 //TODO: connect to db
 //TODO: change morgan to run as dev or as prod
 const app = new App(Number(process.env.API_PORT), morgan('dev'), []);
